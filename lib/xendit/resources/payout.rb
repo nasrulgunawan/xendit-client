a0b6744124b2d569d88cc0d09168962e23004a2a
Nasrul Gunawan <search.nasrul@gmail.com>
2023-10-09 22:48:57 +0700

    Add payouts

--- lib/xendit/resources/payout.rb
+++ lib/xendit/resources/payout.rb
@@ -6,6 +6,22 @@ module Xendit
       def channels(**params)
         ApiClient.get "payouts_channels", params
       end
+
+      def create(params)
+        ApiClient.post "v2/payouts", params
+      end
+
+      def cancel(id)
+        ApiClient.post "v2/payouts/#{id}/cancel"
+      end
+
+      def get_by_id(id)
+        ApiClient.get "v2/payouts/#{id}"
+      end
+
+      def get_by_reference_id(reference_id)
+        ApiClient.get "v2/payouts", {reference_id: reference_id}
+      end
     end
   end
 end
